ef4792778eeae518f369d0e22aa454f2f73d9b94
Alex Kessinger <alex@mixedmedialabs.com>
2012-09-06 23:09:58 -0700

    breaking up fab file some more

--- fabfile.py
+++ fabfile.py
@@ -3,16 +3,20 @@ from os import environ
 from fabric.api import local,  settings
 
 
-def deploy():
+def build_site():
     environ['FLASK_CONF'] = './conf/prod.cfg'
     local('python freeze.py')
+
+
+def deploy():
     local(("s3cmd put --exclude '*.webassets-cache*' --exclude '*static/css*' "
            "--exclude '*static/js*' --acl-public --guess-mime-type -r build/ s3://share-on-adn"))
 
 
 def deploy_pages():
     with settings(warn_only=True):
-        local('cp -fR build /tmp/_store_them_files')
+        local('rm -fR /tmp/_store_them_files')
+        local('mv build /tmp/_store_them_files')
         local('git checkout gh-pages')
         local('cp -fR /tmp/_store_them_files/* ./')
         local('git add .')
