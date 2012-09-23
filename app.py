d52fc0a0fb20442286930007f0fa7ed609eb63c0
Alex Kessinger <alex@mixedmedialabs.com>
2012-09-23 00:18:27 -0700

    Adding inbound email

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import os
+import sys
+import logging
+
 from flask import Flask, render_template
 from flask.ext.assets import Environment, Bundle
+from postmark.inbound import PostmarkInbound
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='"%(asctime)s %(levelname)8s %(name)s - %(message)s"',
+    datefmt='%H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
 
 flask_conf = os.environ.get('FLASK_CONF') or './conf/dev.cfg'
 PORT_NUM = int(os.environ.get('PORT', 5000))
@@ -47,6 +60,12 @@ assets.register('css_all', js)
 def hello():
     return render_template('index.html', ROOT_URL=app.config.get('ROOT_URL'))
 
+
+@app.route("/inbound/email")
+def inbound_email(request):
+    inbound = PostmarkInbound(json=request.data)
+    logger.INFO(inbound.json)
+    return 'ok'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=PORT_NUM)
