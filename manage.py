291b9cbb895064048de3761b05c10fd1031b0f2b
Alex Kessinger <alex@mixedmedialabs.com>
2012-12-24 01:40:03 -0800

    How the fuck do I do this in the future

--- manage.py
+++ manage.py
@@ -2,17 +2,11 @@
 from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
 
-from app import app, assets, User, Post
+from app import app, assets
 
 manager = Manager(app)
 
 manager.add_command("assets", ManageAssets(assets))
-
-
-@manager.command
-def create_database():
-    User.create_table()
-    Post.create_table()
 
 
 if __name__ == "__main__":
