a36ddbf5bab5a50424415685cf8b0b6ecca47e55
Alex Kessinger <alex@mixedmedialabs.com>
2012-12-24 01:26:30 -0800

    Some awesome changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,83 @@
 (function () {
+
+'use strict';
+
+// markdown can have markdown [test](url) links, or bare URLs.  Anything else will be ignored and remain text.  Only link entities are set; that's all that's needed to post to the app.net API.
+var parse = function(markdown) {
+  // Markdown bracket regex based on http://stackoverflow.com/a/9268827
+  var markdownLinkRegex = /\[([^\]]+)\]\((\S+(?=\)))\)/;
+
+  // Regex pulled from https://github.com/chriso/node-validator and country codes pulled from http://data.iana.org/TLD/tlds-alpha-by-domain.txt
+  var bareUrlRegex = /((?:http|https|ftp|scp|sftp):\/\/)?(?:\w+:\w+@)?(?:localhost|(?:(?:[\-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|AC|AD|AE|AF|AG|AI|AL|AM|AN|AO|AQ|AR|AS|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|IO|IQ|IR|IS|IT|JE|JM|JO|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MK|ML|MM|MN|MO|MP|MQ|MR|MS|MT|MU|MV|MW|MX|MY|MZ|NA|NC|NE|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|PA|PE|PF|PG|PH|PK|PL|PM|PN|PR|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|YE|YT|ZA|ZM|ZW))|(?:(?:\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(?:\.(?:\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[\-\w~!$+|.,="'\(\)_\*:]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[\-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[\-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[\-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[\-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[\-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?/ig;
+
+  var links = [];
+  var text = markdown;
+
+  function handleReplacement(_, anchor, url, pos) {
+    links.push({
+      pos: pos,
+      len: anchor.length,
+      url: url
+    });
+
+    return anchor;
+  }
+
+  var oldText;
+
+  // Has to be called repeatedly, since if done globally, it will provide the original index (before earlier replacements)
+  do {
+    oldText = text;
+    text = oldText.replace(markdownLinkRegex, handleReplacement);
+  } while(text !== oldText);
+
+  var match;
+  while((match = bareUrlRegex.exec(text))) {
+    var url = match[0];
+    var len = url.length;
+
+    if(match[1] === undefined) {
+      url = 'http://' + url;
+    }
+
+    links.push({
+      pos: match.index,
+      len: len,
+      url: url
+    });
+  }
+
+  return {
+    text: text,
+    entities: {
+      links: links
+    }
+  };
+};
+
+// Return markdown given app.net object, generally either a post or user description.  Only text and entities.links are currently used
+var stringify = function(adnObject) {
+  var markdown = adnObject.text;
+  var links = adnObject.entities.links;
+  var link;
+  links.sort(function(l1, l2) {
+    return l1.pos - l2.pos;
+  });
+
+  for(var i = links.length - 1; i >= 0; i--) {
+    link = links[i];
+    // We keep bare URLs bare.
+    if(link.text !== link.url) {
+      // https://github.com/appdotnet/api-spec/issues/230
+      var remaining = markdown.substring(link.pos + link.len);
+      remaining = remaining.replace(/^ \[[^\]]+\]/, '');
+      markdown = markdown.substring(0, link.pos) + '[' + link.text + '](' + link.url + ')' + remaining;
+    }
+  }
+
+  return markdown;
+};
+
 var TEMPLATE_CONTEXT = OPTIONS;
 
 var TEMPLATE_CACHE = {};
@@ -135,6 +214,41 @@ var API = function (conf) {
     return $.ajax(conf);
 };
 
+// Length of actual text that will be posted to app.net
+var getEffectiveLength = function (text) {
+    // Same as in markdown-to-entities, but global
+    var markdownLinkRegex = /\[([^\]]+)\]\((\S+(?=\)))\)/g;
+
+    // Apparently newlines are considered part of the character count limitation
+    // And apparently the newline is treated as two characters, not one when passed
+    // to the API as a post message - so here we count one for the key enter and one for
+    // the newline regex match. Which results in working out as two: '\n'
+    var markdownText = text.replace(markdownLinkRegex, '$1');
+    var markdownTextNewlineCount = 0;
+    if (markdownText.match(/\n/g)) {
+        markdownTextNewlineCount = markdownText.match(/\n/g).length;
+    }
+
+    return markdownText.length + markdownTextNewlineCount;
+};
+
+var checkCharLimit = function (text) {
+    var textLength = getEffectiveLength(text);
+    var button = write.find('button');
+
+    if (textLength > CHAR_MAX) {
+        charLimit.addClass('over');
+        charLimit.text('- ' + (textLength - CHAR_MAX));
+        button.attr('disabled', 'disabled');
+        button.addClass('disabled');
+    } else {
+        charLimit.removeClass('over');
+        charLimit.text(CHAR_MAX - textLength);
+        button.removeAttr('disabled');
+        button.removeClass('disabled');
+    }
+};
+
 var PostView = PageView.extend({
     template_name: 'post',
     events: {
@@ -143,7 +257,7 @@ var PostView = PageView.extend({
         'click .js-cancel-post': 'cancel_post_to_adn'
     },
     update_char_count: function () {
-        var char_count = 250 - this.$el.find('.js-post-text').val().length;
+        var char_count = 250 - getEffectiveLength(this.$el.find('.js-post-text').val());
         this.$el.find('.js-char-count').text(char_count);
     },
     post_to_adn: function () {
@@ -151,6 +265,7 @@ var PostView = PageView.extend({
         var embed = carousel.find('.js-embed-carousel-item:not(.hide)');
         var post = this.serialize_form();
         var embeddable_url = post.annotations[0].value.url || post.annotations[0].value.link;
+        var channel = parseInt($('[name="post-to"]').val(), 10);
 
         if (embed.data('source') === 'embed') {
             this.embed.embeddable_url = embeddable_url;
@@ -167,8 +282,13 @@ var PostView = PageView.extend({
 
         var _this = this;
 
+        var url = "/posts?include_annotations=1";
+        if (channel !== 0) {
+            url = '/channels/' + channel + '/messages';
+        }
+
         API({
-            url: "/posts?include_annotations=1",
+            url: url,
             type: "POST",
             data: JSON.stringify(post),
             contentType: "application/json; charset=utf-8",
@@ -186,25 +306,32 @@ var PostView = PageView.extend({
         return false;
     },
     serialize_form: function () {
+        var created_at = new Date();
 
-        return {
-            text: this.$el.find('.js-post-text').val(),
-            annotations: [{
-                type: "net.app.core.oembed",
-                value: {
-                    type: 'rich',
-                    version: '1.0',
-                    html: this.$el.find('.js-snippet').val(),
-                    title: this.$el.find('.js-title').val(),
-                    url: this.$el.find('.js-link').val(),
-                    width: 500,
-                    height: 250
-                }
-            }]
-        };
+        var post = parse(this.$el.find('.js-post-text').val());
+
+        post.annotations = [{
+            type: "net.app.core.oembed",
+            value: {
+                type: 'rich',
+                version: '1.0',
+                html: this.$el.find('.js-snippet').val(),
+                title: this.$el.find('.js-title').val(),
+                url: this.$el.find('.js-link').val(),
+                width: 500,
+                height: 250
+            }
+        }, {
+            type: "net.share-app.item",
+            value: {
+                created: created_at.toISOString()
+            }
+        }];
+
+        return post;
     },
     render_to_form: function (data) {
-        var post_text = data.title + ' ' + data.url;
+        var post_text = data.title + ' [#](' + data.url + ')';
         var snippet = $('<div/>').html(data.snippet).get(0);
         snippet = MySanitizer.clean_node(snippet);
         snippet = $('<div/>').append(snippet.childNodes).get(0).innerHTML;
@@ -231,10 +358,11 @@ var PostView = PageView.extend({
         var carousel_item = this.render_to_html('embed-carousel-item', {embed: embed});
         carousel_item_container.append(carousel_item);
 
-        carousel_view = new CarouselView({el: carousel});
+        var carousel_view = new CarouselView({el: carousel});
     },
     initialize: function () {
         var _this = this;
+        this.options.channels = JSON.parse(localStorage.channels || '[]');
         this.render();
 
         this.host_url = getQueryVariable('host_url');
@@ -249,6 +377,30 @@ var PostView = PageView.extend({
         }, false);
 
         window.top.postMessage({'method': 'iframe_ready'}, this.host_url);
+
+        API({
+            url: "/users/me",
+            type: "GET",
+            data: {
+                include_annotations: 1
+            }
+        }).done(function (data) {
+            if (data.data.annotations) {
+                _.each(data.data.annotations, function (annotation) {
+                    if (annotation.type == "net.share-app.feed-ad") {
+                        localStorage.channels = JSON.stringify(annotation.value.list);
+                        var select = $('[name="post-to"]');
+                        select.html('');
+                        select.append('<option value=0>Feed</option>');
+                        _.each(annotation.value.list, function (channel) {
+                            select.append('<option value="' + channel.channel + '">' + channel.name + '</option>');
+                        });
+                        return false;
+                    }
+                });
+            }
+
+        });
     }
 });
 
